e7c5e9dfb045cd1d42a9ecafb576fef5078c71cd
Rick3Mrtz <Rick.mrtz3@gmail.com>
2023-03-03 01:05:53 -0600

    styled About Me

--- portfolio/src/components/Section.js
+++ portfolio/src/components/Section.js
@@ -6,13 +6,13 @@ import '../styles/Section.css'
 function Section () {
     return (
         <section className='section'>
-            <div className='mx-[15rem] py-20'>
-                <div className=' grid grid-cols-2 gap-[15rem]'>
-                    <div className='text-center text-black text-[7rem] leading-[7rem]'>
+            <div className='mx-[13rem] py-20'>
+                <div className=' grid grid-flow-row-dense grid-cols-3 gap-[4rem]'>
+                    <div className='about-me col-span-2 relative text-center text-white text-[5.5rem] leading-[7rem] bg-gradient-to-b from-purple-900 to-blue-400 rounded-full w-80 h-100'>
                         About Me 
                     </div>
-                    <div className='mt-20 text-lg text-right'>
-                        From working with hundreds of healthcare workers the past 6 years, I am now ready to work with hundreds of Web Developers!
+                    <div className='mt-12 text-[18px] text-center mx-2'>
+                        From working with hundreds of healthcare workers the past 6 years, now I'm looking forward to working with hundreds of Web Developers!
                     </div>
                 </div>
             </div>
