332b7f924c6579b49cc7d2d1c8db54ecf67b9294
Rick3Mrtz <Rick.mrtz3@gmail.com>
2023-03-06 06:33:13 -0600

    animations added to section.js using gsap

--- portfolio/src/components/Education.js
+++ portfolio/src/components/Education.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { scrollTrigger } from 'gsap/ScrollTrigger';
 
 
 function Education () {
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    const el = divRef.current;
+    gsap.fromTo( el,
+      { 
+        y: 50,
+        opacity: 0,     
+      },
+      { 
+        y: 0,
+        opacity: 1,
+        duration: 2,
+        scrollTrigger: {
+          trigger: el,
+          start: "top 70%"
+        },
+      }
+      );
+  }, []);
+
+
+
     return (
         <div className='bg-purple-900'>
             <div class="flex-1 bg-purple-900 text-[5rem] flex items-center h-[22vh] justify-center">
           <h1 className='text-transparent bg-clip-text bg-gradient-to-r from-purple-700 to-red-400 mb-8 tracking-wider'>Education</h1>
         </div>
-        <div class="flex-1 bg-white flex items-center justify-center rounded-[4rem] shadow-2xl mx-[20rem]">
+        <div ref={divRef} class="flex-1 bg-white flex items-center justify-center rounded-[4rem] shadow-2xl mx-[20rem]">
           <h1 class="text-purple-600 text-center text-[3rem] my-6 mx-20">University of Texas at Austin<br></br>
           Full Stack Developer Bootcamp<br></br>
           Class of 2023</h1>
--- portfolio/src/components/Section.js
+++ portfolio/src/components/Section.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { scrollTrigger } from 'gsap/ScrollTrigger';
 
 import '../styles/Section.css'
 
 import { ScrollTrigger } from 'gsap/all';
+gsap.registerPlugin(ScrollTrigger);
 
 
 function Section () {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const el = containerRef.current;
+    gsap.fromTo( el, 
+      { x: 1000 },
+      {
+
+        x: 0,
+        duration: 3,
+        scrollTrigger: {
+          trigger: el,
+          start: "top 55%"
+          
+        },
+      }
+    );
+  }, []);
+
+
     return (
         <body id="about-me" class="flex flex-row h-[60vh]">
         
         <div class="flex-1 bg-white flex items-center justify-center">
           <h1 class="text-purple-800 text-right text-[6rem] leading-[5.4rem] border-l-[1rem] border-b-[1rem] tracking-wider">Here is<br></br>a little <br></br>about<br></br><span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-700 to-red-400'>Myself</span></h1>
         </div>
-        <div class="flex-1 bg-purple-800 flex items-center justify-center rounded-[4rem] shadow-2xl mr-40 overscroll-contain">
+        <div ref={containerRef} class="about-me flex-1 bg-purple-800 flex items-center justify-center rounded-[4rem] shadow-2xl mr-40 overscroll-contain">
           <h1 class="text-white text-center text-2xl mx-16 flex flex-wrap">This is the about me section this is the about me section This is the about me section this is the about me section This is the about me section this is the about me section This is the about me section this is the about me section This is the about me section this is the about me section</h1>
 
 
